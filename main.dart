dc7e370ed8692ee5f05bd07f809ffb98abeaaf46
Rohansurya2580 <110003736+Rohansurya2580@users.noreply.github.com>
2022-10-19 00:56:11 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,41 @@
+// @dart=2.9
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:pdftoolapplicario/screens/doc_scan.dart';
+import 'package:pdftoolapplicario/screens/first_screen.dart';
+import 'package:pdftoolapplicario/screens/pdf_merge.dart';
+
+import 'screens/landing_page.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness:
+      Platform.isAndroid ? Brightness.dark : Brightness.light,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.grey,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      title: '',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        platform: TargetPlatform.iOS,
+          primaryColor: Color(0xfff8f5f0)
+      ),
+      home: LandingPage(),
+    );
+  }
+}
